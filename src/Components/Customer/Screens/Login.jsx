c4cf6475f3fd82121a0c551e9c677c75db5826ca
Ajaj0000 <ajajkhan2842@gmail.com>
2025-06-30 12:09:50 +0530

    change

--- src/Components/Customer/Screens/Login.jsx
+++ src/Components/Customer/Screens/Login.jsx
@@ -2,39 +2,48 @@ import React from 'react';
 import logo from '../../../Assets/notification_icon.png'; // Replace with your logo path
 
 const Login = () => {
+
+    // mobileNumber
+    const sendOtp =async()=>{
+        try {
+            const res = await fetch(`${process.env.REACT_APP_BASE_URL}/v1/api/loginUserByOtp`,{
+                method:"POST",
+                headers:{
+                    'Content-Type' : 'application/json'
+                }
+            })
+        } catch (e) {
+            console.log(e,"error in login api");
+        }
+    }
+
     return (
         <div className="min-vh-100 d-flex flex-column bg-white">
-            {/* Top Nav */}
-
-
-            {/* Centered Content */}
             <div className="flex-grow-1 d-flex justify-content-center align-items-center">
                 <div className="text-center" style={{ width: '100%', maxWidth: 350 }}>
-
-
                     <div className="bg-white rounded-4 shadow">
 
-                        <div className="promo-header them-bg-black d-flex align-items-center justify-content-center mb-2" style={{ borderBottomRightRadius: 0, borderBottomLeftRadius: 0,borderTopLeftRadius:"10px",borderTopRightRadius:"10px" }}>
+                        <div className="promo-header them-bg-black d-flex align-items-center justify-content-center mb-2" style={{ borderBottomRightRadius: 0, borderBottomLeftRadius: 0, borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }}>
                             <h5 className="text-warning m-auto">Login</h5>
                             <span></span>
                         </div>
-<div className="p-4">
-                        <img
-                            src={logo}
-                            alt="Logo"
-                            className="mb-4"
-                            style={{ width: '180px', height: '180px', objectFit: 'contain' }}
-                        />
+                        <div className="p-4">
+                            <img
+                                src={logo}
+                                alt="Logo"
+                                className="mb-4"
+                                style={{ width: '180px', height: '180px', objectFit: 'contain' }}
+                            />
 
-                        <input
-                            type="text"
-                            className="form-control mb-3 rounded-pill text-center "
-                            placeholder="Mobile No."
-                        />
-                        <button className="btn btn-dark w-100 rounded-pill fw-bold ">
-                            LOGIN
-                        </button>
-</div>
+                            <input
+                                type="text"
+                                className="form-control mb-3 rounded-pill text-center "
+                                placeholder="Mobile No."
+                            />
+                            <button className="btn btn-dark w-100 rounded-pill fw-bold ">
+                                LOGIN
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -9,7 +9,7 @@ import AuthGate from "../Utilities/AuthGate";
 function Routing() {
 
     return (
-        <LocationGate>
+        // <LocationGate>
             <Routes>
                 <Route path="/" element={<AuthGate/>}/>
                 <Route path="/home" element={<Restaurants />} />
@@ -18,7 +18,7 @@ function Routing() {
 
                 {/* <Route path="/cafeMenu" element={<Home />} /> */}
             </Routes>
-        </LocationGate>
+        // </LocationGate>
     )
 }
 export default Routing;
